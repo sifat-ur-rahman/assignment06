f5b407e2035c334627e31918afe7d77f13698aaa
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-09-02 22:33:36 +0600

    url

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('add')
+const loadData = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayElement(data.data.news_category)
+}
+
+const displayElement = (categorys) => {
+    const clickElement = document.getElementById('click-element')
+    categorys.forEach(category => {
+        console.log(category)
+    });
+}
+
+
+loadData()
